f8ea761c35c2aab791b86a369be2a93c2beced68
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-28 18:37:44 -0300

    Fetch and render snippets from the database in the Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import { db } from "@/db";
+
+export default async function Home() {
+
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h3>{snippet.title}</h3>
+        <p>{snippet.code}</p>
+      </div>
+    )
+  })
+
   return (
-    <div>Test</div>
+    <div>
+      {renderedSnippets}
+    </div>
   );
 }
