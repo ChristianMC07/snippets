14c44e1a515653162be059b98b035ec08e2ada5d
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-28 19:34:11 -0300

    Refactor SnippetShowPage to await params

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,15 +2,18 @@ import { notFound } from "next/navigation";
 import { db } from "@/db";
 
 interface SnippetShowPageProps {
-    params: {
-        id: string;
-    }
+    params: Promise<{
+        id: string
+    }>
 }
+
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
+    const { id } = await props.params;
+
     const snippet = await db.snippet.findFirst({
-        where: { id: parseInt(props.params.id) }
+        where: { id: parseInt(id) }
     })
 
     if (!snippet) {
