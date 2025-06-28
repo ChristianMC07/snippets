d703be450c9fda2a5d37c857d6b945f220ccc753
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-28 18:23:46 -0300

    Implement server action for snippet creation and update form submission handling

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,32 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+
+    async function createSnippet(formData: FormData) {
+        //make clear to NextJS that this is a server action
+        'use server';
+        //check user's inputs and validate them
+        const title = formData.get('title') as string;
+        const code = formData.get('code') as string;
+        //create a new record in the DB
+
+        const snippet = await db.snippet.create({
+            data: {
+                title: title,
+                code: code
+            }
+        })
+
+        console.log(snippet);
+
+        //redirect the user back to the root route
+        redirect('/');
+    }
+
+
     return (
-        <form>
+        <form action={createSnippet}>
             <h3 className="font-bold mb-4">Create a Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
