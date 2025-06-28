b3cf461a984dc320d1f9a9b2e48cfbcf161a2fd7
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-28 17:55:49 -0300

    Implement Snippet creation form with title and code input fields

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,19 @@
 export default function SnippetCreatePage() {
     return (
-        <div>Create a Snippet</div>
+        <form>
+            <h3 className="font-bold mb-4">Create a Snippet</h3>
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-4">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" id="title" name="title" className="border rounded p-2 w-full" />
+                </div>
+                <div className="flex gap-4">
+                    <label htmlFor="code">Code</label>
+                    <input type="textarea" id="code" name="code" className="border rounded p-2 w-full" />
+                </div>
+
+                <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
+            </div>
+        </form>
     )
 }
