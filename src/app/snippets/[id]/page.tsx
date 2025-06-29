257a3021526fb188d7fb3a931cb63b08a54fe254
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-29 14:32:17 -0300

    Enhance SnippetShowPage layout with title, edit, and delete buttons, and improved code display

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -21,6 +21,17 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     }
 
     return (
-        <div>{snippet.title}</div>
+        <div>
+            <div className="flex m-4 justify-between items-center">
+                <h1 className="text-xl font-bold">{snippet.title}</h1>
+                <div className="flex gap-4">
+                    <button className="p-2 border rounded">Edit</button>
+                    <button className="p-2 border rounded">Delete</button>
+                </div>
+            </div>
+            <pre className="m-4 p-4 border rounded bg-gray-200 border-gray-200">
+                <code className="text-amber-800">{snippet.code}</code>
+            </pre>
+        </div>
     );
 }
