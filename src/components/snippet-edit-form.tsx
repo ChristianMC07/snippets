3e7bc53477d23b661f8446a6e1ad66ea655ffc24
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-29 17:11:05 -0300

    Implement code editor in SnippetEditForm using Monaco Editor for snippet editing

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,14 +1,36 @@
 'use client'
 import type { Snippet } from '@prisma/client'
+import Editor from '@monaco-editor/react'
+import { useState } from 'react'
+
 
 interface SnippetEditFormProps {
     snippet: Snippet
 }
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+
+    const [code, setCode] = useState(snippet.code);
+
+    const handleEditorChange = (value: string = "") => {
+        setCode(value);
+    }
     return (
         <div>
-            Client Component, snippet with title {snippet.title}
+            <Editor
+                height="50vh"
+                language="javascript"
+                theme="vs-dark"
+                defaultValue={code}
+                options={
+                    {
+                        minimap: {
+                            enabled: false
+                        },
+                    }
+                }
+                onChange={handleEditorChange}
+            />
         </div>
     )
 }
