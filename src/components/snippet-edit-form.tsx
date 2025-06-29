1b5c74b482420566b99fd724465c70eee816b971
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-29 15:31:59 -0300

    Add SnippetEditForm client component to handle snippet editing

---
+++ src/components/snippet-edit-form.tsx
@@ -0,0 +1,14 @@
+'use client'
+import type { Snippet } from '@prisma/client'
+
+interface SnippetEditFormProps {
+    snippet: Snippet
+}
+
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+    return (
+        <div>
+            Client Component, snippet with title {snippet.title}
+        </div>
+    )
+}
