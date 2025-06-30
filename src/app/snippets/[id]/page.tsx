ddaf2889956086b28bd40141bd1c3d0a13bcb6dc
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-29 21:19:30 -0300

    Bind deleteSnippet action to the delete button in SnippetShowPage for snippet deletion functionality

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { db } from "@/db";
 import Link from "next/link";
+import * as actions from '@/actions/actions'
 
 interface SnippetShowPageProps {
     params: Promise<{
@@ -21,13 +22,17 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
         return notFound();
     }
 
+    const deleteSnippetAction = actions.deleteSnippet.bind(null, parseInt(id));
+
     return (
         <div>
             <div className="flex m-4 justify-between items-center">
                 <h1 className="text-xl font-bold">{snippet.title}</h1>
                 <div className="flex gap-4">
                     <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-                    <button className="p-2 border rounded">Delete</button>
+                    <form action={deleteSnippetAction}>
+                        <button className="p-2 border rounded">Delete</button>
+                    </form>
                 </div>
             </div>
             <pre className="m-4 p-4 border rounded bg-gray-200 border-gray-200">
