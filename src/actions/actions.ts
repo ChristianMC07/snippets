640511ba26a7241b88655b06c27e0a955dbbe833
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-06-30 19:25:33 -0300

    Add createSnippet action to handle snippet creation and integrate it into SnippetCreatePage for user input validation

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -17,3 +17,26 @@ export async function deleteSnippet(id:number){
     })
     redirect('/');
 }
+
+export async function createSnippet(formState:{message:string}, formData: FormData) {
+
+   return{
+    message: 'Title must be longer'
+   };
+   
+    // const title = formData.get('title') as string;
+    // const code = formData.get('code') as string;
+    // //create a new record in the DB
+
+    // const snippet = await db.snippet.create({
+    //     data: {
+    //         title: title,
+    //         code: code
+    //     }
+    // })
+
+    // console.log(snippet);
+
+    // //redirect the user back to the root route
+    // redirect('/');
+}
