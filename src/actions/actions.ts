2e75981d9c16204c0341705cfd8ae7ee9547d2c5
ChristianMC07 <christian.marcelo.castro@gmail.com>
2025-07-02 10:43:33 -0300

    Refactor createSnippet action to include error handling for title and code validation; ensure proper error messages are returned on failure

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -18,33 +18,39 @@ export async function deleteSnippet(id:number){
     redirect('/');
 }
 
-export async function createSnippet(formState:{message:string}, formData: FormData) {
-   
-    const title = formData.get('title');
-    const code = formData.get('code');
+export async function createSnippet(formState: { message: string }, formData: FormData) {
+    try {
+        const title = formData.get('title');
+        const code = formData.get('code');
 
-    if(typeof title !== 'string' || title.length<3){
-        return{
-            message: 'Title must be longer'
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                message: 'Title must be longer'
+            };
+        }
+        if (typeof code !== 'string' || code.length < 10) {
+            return {
+                message: 'Code must be longer'
+            };
+        }
+
+        // create a new record in the DB
+        await db.snippet.create({
+            data: {
+                title: title,
+                code: code
+            }
+        });
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return {
+                message: error.message
+            };
+        } else {
+            return {
+                message: 'Something went wrong'
+            };
         }
     }
-    if(typeof code !=='string' || code.length<10 ){
-        return {
-            message:'Code must be longer'
-        }
-    }
-    
-    //create a new record in the DB
-
-    const snippet = await db.snippet.create({
-        data: {
-            title: title,
-            code: code
-        }
-    })
-
-    console.log(snippet);
-
-    //redirect the user back to the root route
     redirect('/');
 }
